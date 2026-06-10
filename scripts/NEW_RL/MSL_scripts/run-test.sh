27171bab4e10016fa14070a0adca94fcc6145258
arimogi <arimogi@gmail.com>
2026-06-10 15:10:19 +0700

    Change the experiment script.

--- scripts/NEW_RL/MSL_scripts/run-test.sh
+++ scripts/NEW_RL/MSL_scripts/run-test.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 
 python -u run_anomaly.py \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/MSL/ \
   --model_id MaelNetS2_AnomalyTransformer_DCDetector_RL_TA\
   --model DCDetector \
@@ -25,35 +25,10 @@ python -u run_anomaly.py \
   --des 'TA' \
   --p_hidden_dims 128 128 \
   --p_hidden_layers 2 \
-  --itr 1 &
+  --itr 100
 
 python -u run_anomaly.py \
-  --is_training 1 \
-  --root_path ./dataset/MSL/ \
-  --model_id MaelNetS2_AnomalyTransformer_DCDetector_RL_TA\
-  --model MaelNetS2 \
-  --is_slow_learner true \
-  --data MSL \
-  --e_layers 3 \
-  --d_layers 1 \
-  --anomaly_ratio 0.85 \
-  --d_ff 512 \
-  --dropout 0.0 \
-  --factor 5 \
-  --enc_in 55 \
-  --dec_in 55 \
-  --c_out 55 \
-  --d_model 512 \
-  --moving_avg 100 \
-  --win_size 100 \
-  --gpu 0 \
-  --des 'TA' \
-  --p_hidden_dims 128 128 \
-  --p_hidden_layers 2 \
-  --itr 1 &
-  
-python -u run_anomaly.py \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/MSL/ \
   --model_id MaelNetS2_AnomalyTransformer_DCDetector_RL_TA\
   --model AnomalyTransformer \
@@ -75,5 +50,32 @@ python -u run_anomaly.py \
   --des 'TA' \
   --p_hidden_dims 128 128 \
   --p_hidden_layers 2 \
-  --itr 1 &
+  --itr 100
+
+python -u run_anomaly.py \
+  --is_training 0 \
+  --root_path ./dataset/MSL/ \
+  --model_id MaelNetS2_AnomalyTransformer_DCDetector_RL_TA\
+  --model MaelNetS2 \
+  --is_slow_learner true \
+  --data MSL \
+  --e_layers 3 \
+  --d_layers 1 \
+  --anomaly_ratio 0.85 \
+  --d_ff 512 \
+  --dropout 0.0 \
+  --factor 5 \
+  --enc_in 55 \
+  --dec_in 55 \
+  --c_out 55 \
+  --d_model 512 \
+  --moving_avg 100 \
+  --win_size 100 \
+  --gpu 0 \
+  --des 'TA' \
+  --p_hidden_dims 128 128 \
+  --p_hidden_layers 2 \
+  --itr 100
+  
+
 
